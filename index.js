2b8ad1ebe957e0ee02c677e01789076a90b8c9d4
Golden-Shark <prusovnik240304@gmail.com>
2025-03-23 14:49:39 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+window.addEventListener("DOMContentLoaded", () => {});
+
+const iconHeader  = document.querySelector('.icon-header');
+const logo = document.querySelector('.logo');
+const nav = document.querySelector('nav');
+const topHeader = document.querySelector('.container');
+
+let marginIcon = parseFloat(getComputedStyle(topHeader).width)/2 - parseFloat(getComputedStyle(iconHeader).width) - parseFloat(getComputedStyle(logo).width)/2;
+iconHeader.style.marginRight = marginIcon + 'px';
+
+let marginLogo = parseFloat(getComputedStyle(topHeader).width)/2 - parseFloat(getComputedStyle(nav).width) - parseFloat(getComputedStyle(logo).width)/2;
+logo.style.marginRight = marginLogo + 'px';
